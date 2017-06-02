800bafaf3ee9a9e651d81eef7cf8873259e9707f
Michail <michaildrazdovich@gmail.com>
2017-06-02 12:04:03 +0300

    init commit

---
+++ android/src/com/awesome/jackgiant/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.awesome.jackgiant;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.awesome.jackgiant.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
